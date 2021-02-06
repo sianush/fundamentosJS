3814569b8b6b1d72e3d197e212c929f2575a721c
Siana <sianush.sian@gmail.com>
2021-02-05 20:51:21 -0600

    clase 10

--- practica/js/clase1.js
+++
@@ -1,8 +0,0 @@
-var nombre = 'Siana'
-
-function imprimirNombreEnMayusculas(){
-    nombre = nombre.toUpperCase()
-    console.log(nombre)
-}
-imprimirNombreEnMayusculas()
-//nombr en mayusculas con un avariable global 
